1fbb0603dd157ad2ae490b069f9c05098f6b3d0f
Илья <a@iteko.su>
2019-09-18 21:14:03 +0300

    1

--- main/views.py
+++ main/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+
+def index(request):
+    template = loader.get_template('main/index.html')
+    context = {
+        #'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def about(request):
+    template = loader.get_template('main/about.html')
+    context = {
+        #'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
